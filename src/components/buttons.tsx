f098f544053659625de3f4a04ec588656dcb3882
syndercombecreswell <syndercombecreswell@gmail.com>
2022-11-12 00:17:37 +0000

    baby buttons

--- src/components/buttons.tsx
+++ src/components/buttons.tsx
@@ -1,5 +1,6 @@
 
 import Data from './babyNamesData.json'
+import {useState} from "react"
 
 interface BabyNamesProps {
     id: number;
@@ -10,16 +11,26 @@ const BabyNamesArr: BabyNamesProps [] = Data;
 
 
 export default function NameButtons(){
+
+    const [name,setName] = useState('');
+
+    const addName = (name: string) =>
+{
+    setName(prev => prev + name);
+}
+
     return(
         <div className="nameButtons">
+            <input value={name}/>
             {
-                Data.map( baby => {
+                    Data.map( baby => {
                     return(
                         <div className="button" key={baby.id}>
-                            <strong>{baby.name}</strong>
+                            <button onClick={() => addName(baby.name)}>{baby.name}</button>
                         </div>
                     )
                 })
+                
             }
         </div>
     );
